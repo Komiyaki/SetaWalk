0a1893b4645f0495c5281c0cfe8fcd2a27daa05b
Komiyaki <kevinbeutler2003@gmail.com>
2026-03-31 14:23:00 +0900

    Added supabase_flutter dependency and initialized Supabase in main.dart. Also added environment variables for Supabase URL and Anon Key in env.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'app.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: String.fromEnvironment('supabaseUrl'),
+    anonKey: String.fromEnvironment('supabaseAnonKey'),
+  );
   runApp(const SetaWalkApp());
 }
+
+final supbase = Supabase.instance.client;
